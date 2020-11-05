3f89e57f857a936f79c01e993791808705312c5d
irfaan888 <irfaan.husen888@gmail.com>
2020-11-04 22:02:05 -0500

    sixth commit

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello
 {
  public static void main(String args[])
  {
-   System.out.println("poll scm  and modularized deployment working!!!!!!");
+   System.out.println("1.poll scm  and 2.modularized deployment working!!!!!!");
  }
 }
